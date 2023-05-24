96f77f4a1e2ac71f9c827d23841f3a0c7b72f47b
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-05-24 14:22:18 +0300

    update filter data

--- src/compoments/Dropdown.js
+++ src/compoments/Dropdown.js
@@ -2,7 +2,7 @@ function Dropdown({ question, name, options, handleChane }) {
   return (
     <div className="dropdown">
       <label htmlFor="option">{question}</label>
-      <select name={name} id="option" onChange={handleChane}>
+      <select className="option" name={name} id="option" onChange={handleChane}>
         <option value="">Please choose an option</option>
         {options.map((option) => (
           <option value={option}>{option}</option>
